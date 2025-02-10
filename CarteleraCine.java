2fef7d3891227421e42a97711137aa95de11e1e8
Lesma <diegolesmamartin@gmail.com>
2025-02-10 23:19:00 +0100

    culero

---
+++ CarteleraCine.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class CarteleraCine {
+    private String nombreCine;
+    private List<Pelicula> peliculas = new ArrayList<>();
+    public CarteleraCine(String cine, Pelicula[] peliculas) {
+        this.nombreCine = cine;
+        for (Pelicula p : peliculas){
+            this.peliculas.add(p);
+        }
+    }
+    public peliculasPorGenero(Pelicula[] peliculas, String genero){
+           // List<Pelicula> resultado = new ArrayList<>();
+           // for (Pelicula pelicula : this.peliculas) {
+           //     if (pelicula.getGenero().equalsIgnoreCase(genero)) {
+           //         resultado.add(pelicula);
+           //     }
+           // }
+           // return resultado;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Cine: "+this.nombreCine+"\nPeliculas en cartelera: "+this.peliculas;
+    }
+    public static void main(String ...args) {
+        Pelicula peliculas[] = { new Pelicula("Perfect days", "Drama", 2023),
+                new Pelicula("Inception", "Accion", 2010),
+                new Pelicula("Jumanji", "Aventura", 1995) };
+        CarteleraCine cinesTelmo = new CarteleraCine("Telmo", peliculas);
+        System.out.println(cinesTelmo);
+    }
+}
