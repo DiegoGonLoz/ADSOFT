3d5f293bd00a265172f18258dd0f820a3b5ccbf3
DiegoGonloz <diegoglgonloz@gmail.com>
2025-03-12 00:12:50 +0100

    Putito

---
+++ branches/Branch.java
@@ -0,0 +1,27 @@
+package branches;
+
+import commits.Commit;
+import java.util.List;
+
+public class Branch {
+    private final String name;
+    private List<Commits>;
+
+    public Branch(String name){
+        this.name = name;
+
+    }
+
+    public ... commit(Commit c){
+
+    }
+
+    public List<Commit> getCommits(){
+
+    }
+
+    @Override
+    public String toString(){
+
+    }
+}
