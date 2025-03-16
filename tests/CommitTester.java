07321a1efb41d8c7c98889ed8fea452ccc0d3d31
Lesma <diegolesmamartin@gmail.com>
2025-03-16 10:29:56 +0100

    Putito

--- tests/CommitTester.java
+++ tests/CommitTester.java
@@ -1,9 +1,22 @@
 package tests;
 
-import changes.Change;
+import changes.*;
 import commits.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static tests.ChangeTester.createChanges;
+
 
 public class CommitTester {
     public static void main (String[] args) {
+
+        ChangeCommit changeCommit1 = new ChangeCommit("Pepe", "First Commit", createChanges());
+        ChangeCommit changeCommit2 = new ChangeCommit("Fernando", "Second Commit", createChanges());
+        List<Commit> commits = Arrays.asList(changeCommit1, changeCommit2);
+        MergeCommit mergeCommit = new MergeCommit(commits);
+
+        mergeCommit.toString();
     }
 }
