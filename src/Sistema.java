598ae511de3ab5d0325d533fbb73f18c60ecfbb4
Lesma <diegolesmamartin@gmail.com>
2025-04-01 23:40:26 +0200

    Bombardeen Adsoft

--- src/Sistema.java
+++ src/Sistema.java
@@ -3,8 +3,19 @@ import proyectos.*;
 import announcements.*;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Sistema {
+    List<Proponente> proponentes;
+    List<ProyectoParticipativo> proyectos;
+    List<Announcement> anuncios;
+
+    public Sistema(List<Proponente> proponentes, List<ProyectoParticipativo> proyectos, List<Announcement> anuncios) {
+        this.proponentes = proponentes;
+        this.proyectos = proyectos;
+        this.anuncios = anuncios;
+    }
+
     public boolean existeCiudadano(String nif){
         return false;
     }
@@ -22,14 +33,26 @@ public class Sistema {
     }
 
     public List<ProyectoParticipativo> proyectosRegistrados(){
-        return null;
+        return this.proyectos;
     }
 
     public ProyectoParticipativo obtenerProyecto(String nombre){
-        return null;
+        for(ProyectoParticipativo p : proyectos){
+            if(Objects.equals(p.getTitulo(), nombre)){
+                return p;
+            }
+        }
+        throw new IllegalArgumentException("El proyecto no existe");
     }
 
     public ProyectoParticipativo obtenerProyecto(int id){
-        return null;
+        for(ProyectoParticipativo p : proyectos){
+            if(Objects.equals(p.getCodigo(), id)){
+                return p;
+            }
+        }
+        throw new IllegalArgumentException("El proyecto no existe");
     }
+
+
 }
--- src/proyectos/ProyectoParticipativo.java
+++ src/proyectos/ProyectoParticipativo.java
@@ -29,9 +29,17 @@ public class ProyectoParticipativo implements FollowedEntity {
         this.apoyos = apoyos;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
     public void apoyar(EnteCiudadano ente) {
         if(apoyoPosible(ente)){
-            apoyos.removeIf(ente2 -> ente.esMiembro(ente2));
+            apoyos.removeIf(ente::esMiembro);
             apoyos.add(ente);
         } else {
             throw Exception
