f8de9f94c68c9591c9c3e14219162cfaa3ced058
DiegoGonloz <diegoglgonloz@gmail.com>
2025-04-01 17:17:55 +0200

    Putito

--- src/proponentes/EnteCiudadano.java
+++ src/proponentes/EnteCiudadano.java
@@ -1,6 +1,10 @@
 package proponentes;
 
 import announcements.Follower;
+import announcements.FollowedEntity;
+
+import java.util.List;
 
 public abstract class EnteCiudadano extends Proponente implements Follower {
+
 }
