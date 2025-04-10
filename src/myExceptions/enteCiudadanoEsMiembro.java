9ebcc385a517df26bb8520c8b34830ac79a073e2
Lesma <diegolesmamartin@gmail.com>
2025-04-11 01:43:13 +0200

    Bombardeen Adsoft

--- src/myExceptions/enteCiudadanoEsMiembro.java
+++ src/myExceptions/enteCiudadanoEsMiembro.java
@@ -1,10 +1,21 @@
 package myExceptions;
 
+/**
+ * Clase tipo excepcion que ocurre al intentar anadir un miembro ya existente
+ */
 public class enteCiudadanoEsMiembro extends IllegalArgumentException {
+    /**
+     * Constructor de enteCiudadanoEsMiembro
+     * @param message mensaje que se le pasa al constructor
+     */
     public enteCiudadanoEsMiembro(String message) {
         super(message);
     }
 
+    /**
+     * Metodo toString de la clase enteCiudadanoEsMiembro
+     * @return string con la información de error
+     */
     @Override
     public String toString() {
         return this.getMessage()+
