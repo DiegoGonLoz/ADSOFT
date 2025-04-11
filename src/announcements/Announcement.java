5d5a98fd151abb5583cab17c0452bcbba948174d
Lesma <diegolesmamartin@gmail.com>
2025-04-11 05:21:22 +0200

    Bombardeen Adsoft

--- src/announcements/Announcement.java
+++ src/announcements/Announcement.java
@@ -1,8 +1,19 @@
 package announcements;
 
+/**
+ * Clase que representa un anuncio
+ *
+ * @author Diego Gonzalez
+ */
 public class Announcement implements Comparable<Announcement> {
+    /**Mensaje a anunciar*/
     private final String message;
 
+    /**
+     * Constructor de la clase Announcement
+     * @param message mensaje a mandar
+     * @throws NullPointerException no se admiten nulls
+     */
     public Announcement(String message) throws NullPointerException {
         if(message == null){
             throw new NullPointerException("Error en el constructor Announcement: Message es null");
@@ -11,6 +22,11 @@ public class Announcement implements Comparable<Announcement> {
         this.message = message;
     }
 
+    /**
+     * Metodo compareTo
+     * @param o the object to be compared.
+     * @return negativo, 0 y positivo según la comparación
+     */
     @Override
     public int compareTo(Announcement o) {
         return this.message.compareTo(o.message);
