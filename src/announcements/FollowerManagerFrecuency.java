59e2d2e91b2bfafea0f969a3b401a8ca2aabcf03
Lesma <diegolesmamartin@gmail.com>
2025-04-11 05:43:08 +0200

    Bombardeen Adsoft

--- src/announcements/FollowerManagerFrecuency.java
+++ src/announcements/FollowerManagerFrecuency.java
@@ -1,12 +1,27 @@
 package announcements;
 
+/**
+ * Clase que representa a FollowerManagerFrecuency
+ *
+ * @author Diego Gonzalez
+ */
 public class FollowerManagerFrecuency extends FollowerManager{
+    /**Contador tipo espero*/
     private int contador = 1;
 
+    /**
+     * Constructo de FollowerManagerFrecuency
+     * @param follower follwer
+     * @throws NullPointerException no se admiten nulls
+     */
     public FollowerManagerFrecuency(Follower follower) throws NullPointerException{
         super(follower);
     }
 
+    /**
+     * Metodo announce
+     * @param t objeto a anunciar
+     */
     @Override
     public void announce(Announcement t) {
         if(this.contador == this.umbral){
