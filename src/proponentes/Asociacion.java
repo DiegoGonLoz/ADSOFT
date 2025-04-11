ffbcbd0a65200ca51c306933d54ad07abb23df57
Lesma <diegolesmamartin@gmail.com>
2025-04-11 05:17:22 +0200

    Bombardeen Adsoft

--- src/proponentes/Asociacion.java
+++ src/proponentes/Asociacion.java
@@ -8,11 +8,26 @@ import sistemas.Sistema;
 
 import java.util.*;
 
+/**
+ * Clase que representa a una asociacion
+ *
+ * @author Diego Gonzalez
+ */
 public class Asociacion extends EnteCiudadano implements FollowedEntity {
+    /**Representante de la asociación*/
     private final Ciudadano representante;
+    /**Set de miembros*/
     private final Set<EnteCiudadano> miembros = new HashSet<EnteCiudadano>();
+    /**Set de followers*/
     private final Set<FollowerManager> followers = new HashSet<FollowerManager>();
 
+    /**
+     * Constructor de la clase Asociación
+     * @param nombre nombre de la asociación
+     * @param contraseña contraseña
+     * @param representante representante de la asociación
+     * @throws NullPointerException no se admiten null
+     */
     public Asociacion(String nombre, String contraseña, Ciudadano representante) throws NullPointerException {
         super(nombre, contraseña);
 
@@ -23,6 +38,12 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         this.representante = representante;
     }
 
+    /**
+     * Metodo para inscribir un enteCiudadano
+     * @param ente objeto a inscribir
+     * @return true o false
+     * @throws EnteCiudadanoEsMiembro error al inscribir a un ya miembro
+     */
     public boolean inscribir(EnteCiudadano ente) throws EnteCiudadanoEsMiembro {
         if(ente instanceof Asociacion){
             if(((Asociacion) ente).representante == this.representante && ((Asociacion) ente).cantidadMiembros() == 1){
@@ -37,16 +58,30 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         return miembros.add(ente);
     }
 
+    /**
+     * Metodo para dar de baja a un ciudadano
+     * @param ciudadano ciudadano a dar de baja
+     * @return true o false
+     */
     public boolean darDeBaja(Ciudadano ciudadano){
         return miembros.remove(ciudadano);
     }
 
+    /**
+     * Metodo para proponer proyectos
+     * @param proyecto proyecto a proponer
+     */
     public void proponer(ProyectoParticipativo proyecto){
         if(Sistema.getInstance().proponerProyecto(proyecto)){
             this.apoyar(proyecto);
         }
     }
 
+    /**
+     * Metodo apoyar
+     * @param proyecto proyecto a apoyar
+     * @return true o false
+     */
     @Override
     public boolean apoyar(ProyectoParticipativo proyecto){
         if(super.apoyar(proyecto)){
@@ -57,6 +92,11 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         return false;
     }
 
+    /**
+     * Metodo para saber si un ente es miembro
+     * @param enteCiudadano ente a comparar
+     * @return true o false
+     */
     public boolean esMiembro(EnteCiudadano enteCiudadano){
         if(this.equals(enteCiudadano)){
             return true;
@@ -75,6 +115,10 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         return false;
     }
 
+    /**
+     * Metodo para calcular la cantidad de miembros
+     * @return cantidad de miembros
+     */
     public int cantidadMiembros(){
         int total = 1;
         for(EnteCiudadano ente : miembros){
@@ -87,6 +131,10 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         return total;
     }
 
+    /**
+     * Metodo para obtener a todos los ciudadanos
+     * @return Set de ciudadanos
+     */
     public Set<Ciudadano> todosLosCiudadanos() {
         Set<Ciudadano> ciudadanos = new HashSet<Ciudadano>(List.of(this.representante));
         for(EnteCiudadano ente : miembros){
@@ -96,22 +144,39 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         return ciudadanos;
     }
 
+    /**
+     * Metodo repetido
+     * @return excepción error añadiendo asociación
+     */
     @Override
     public ErrorAnadiendoAsociacionExistente repetido() {
         return new ErrorAnadiendoAsociacionExistente("Asociacion " +this.getNombre()+" ya existente");
     }
 
+    /**
+     * Metodo receives de un anuncio
+     * @param t anuncio a recibir
+     */
     public void receives(Announcement t) {
         for(EnteCiudadano ente : miembros){
             ente.receives(t);
         }
     }
 
+    /**
+     * Metodo toString
+     * @return string con la información importante de la asociación
+     */
     @Override
     public String toString() {
         return this.nombre + " <asociacion con "+this.cantidadMiembros()+" ciudadanos>";
     }
 
+    /**
+     * Metodo equals
+     * @param obj objeto a comparar
+     * @return true o false
+     */
     @Override
     public boolean equals(Object obj) {
         if(obj == null) return false;
@@ -124,21 +189,39 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         return false;
     }
 
+    /**
+     * Metodo hashCode
+     * @return hashCode del objeto
+     */
     @Override
     public int hashCode(){
         return Objects.hash(this.nombre);
     }
 
+    /**
+     * Metodo follow en asociación
+     * @param f follower
+     * @return true o false
+     */
     @Override
     public boolean follow(Follower f) {
         return followers.add(new FollowerManagerAllMessages(f));
     }
 
+    /**
+     * Metodo unfollow en asociación
+     * @param f follower
+     * @return true o false
+     */
     @Override
     public boolean unfollow(Follower f) {
         return followers.remove(f);
     }
 
+    /**
+     * Metodo announce
+     * @param t anuncio por anunciar
+     */
     @Override
     public void announce(Announcement t) {
         for(FollowerManager follower : followers){
@@ -146,6 +229,12 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         }
     }
 
+    /**
+     * Metodo follow con estrategia en asociación
+     * @param f follower
+     * @param ns estrategia a seguir
+     * @return true o false
+     */
     @Override
     public boolean follow(Follower f, AnnouncementStrategy ns) {
         return switch (ns) {
@@ -154,6 +243,12 @@ public class Asociacion extends EnteCiudadano implements FollowedEntity {
         };
     }
 
+    /**
+     * Metodo changeUmbral
+     * @param f follower
+     * @param umbral nuevo umbral
+     * @return true o false
+     */
     public boolean changeUmbral(Follower f, int umbral){
         for(FollowerManager follower : followers){
             if(follower.getFollower().equals(f)){
